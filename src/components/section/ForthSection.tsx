368e9193803fb3dc79da8c3099af670f3d887d85
LeeJaeHo99 <ljh2735294@naver.com>
2025-05-08 23:02:23 +0900

    메인 페이지 완

--- src/components/section/ForthSection.tsx
+++ src/components/section/ForthSection.tsx
@@ -1,7 +1,13 @@
 export default function ForthSection() {
     return (
-        <section className="sectioin sub-section forth-section">
-            <div className="inner"></div>
+        <section className="sectioin forth-section">
+            <div className="inner center column">
+                <h4>
+                    우리 모두가 서로의 다름을 존중하며
+                    <br />
+                    더불어 행복한 하루를 살아가길 바랍니다
+                </h4>
+            </div>
         </section>
     );
 }
